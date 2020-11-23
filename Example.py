c8a28685f5b6acdc7e6fe84e0ed11e8bc2fae66a
schneiderb-projects <59781241+schneiderb-projects@users.noreply.github.com>
2020-11-23 17:30:57 -0500

    Update Example.py

--- Example.py
+++ Example.py
@@ -3,19 +3,16 @@ from Gantry import Gantry
 '''
     program to use the Gantry library in order to control a three axis gantry with X, Y, and Z axis.
     
-    Additional horizontal or vertical axis can be added easily by adding addition IP address to either 
-    the Horizontal or Vertical Axes List. 
+    Additional axes can be added easily by adding addition IP address when initializing
 '''
 
 x_CMMO_ip_address = "172.21.48.20"  # ip address of the CMMO controlling the x axis
 y_CMMO_ip_address = "172.21.48.22"  # ip address of the CMMO controlling the y axis
 z_CMMO_ip_address = "172.21.48.24"  # ip address of the CMMO controlling the z axis
 
-horizontalAxes = [x_CMMO_ip_address, y_CMMO_ip_address]  # list of horizontal CMMO IP's
-verticalAxes = [z_CMMO_ip_address] # list of vertical CMMO IP's
+all_axes = [x_CMMO_ip_address, y_CMMO_ip_address, z_CMMO_ip_address]
 
-gantry = Gantry(horizontalAxes, verticalAxes)  # Gantry(list of horizontal axis CMMO IP Addresses,
-                                               # list of vertical axis CMMO IP Addresses)
+gantry = Gantry(all_axes)  # Gantry(list of CMMO ip addresses)
 gantry.enable()  # Turn on the motors. Must enable the gantry in order to move or home.
 gantry.home()  # Homes all axis of the gantry
 
@@ -31,18 +28,17 @@ def checkDigit(digit_str):  # check if a string can be converted to an int
 
 while True:  # Infinite loop
     inp = input("Input coordinate (x y z) or H (home) or D (disconnect): ") # get user input
+    split_inp = inp.split(" ")
     if 'H' in inp:
         gantry.home()  # Home gantry
-    if 'D' in inp:
+    elif 'D' in inp:
         gantry.disconnect()  # Disconnect from CMMO
         exit(0)
-
-    inp = inp.split(" ")
-    if len(inp) == 3 and checkDigit(inp[0]) and checkDigit(inp[1]) and checkDigit(inp[2]): # check user input
-        x = int(inp[0])
-        y = int(inp[1])
-        z = int(inp[2])
-        gantry.moveTo([x, y], [z])  # moveTo(list of locations for horizontal axis, list of locations for vertical axis)
-                                    # use -1 to keep an axis in the same location
+    elif len(split_inp) == 3 and checkDigit(split_inp[0]) and checkDigit(split_inp[1]) and checkDigit(split_inp[2]): # check user input
+        x = int(split_inp[0])
+        y = int(split_inp[1])
+        z = int(split_inp[2])
+        gantry.moveTo([x, y, z])  # moveTo(list of locations)
+                                  # use -1 to keep an axis in the same location
     else:
         print("invalid input, try again")
